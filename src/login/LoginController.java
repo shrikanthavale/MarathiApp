46b3ec30e56c6e94c26c5a20e8870aa6dc6aad73
Shrikant Havale <shrikant.havale@yahoo.com>
2019-06-04 23:55:07 +0530

    First draft to test UTF-8 encoding MarathiApp

---
+++ src/login/LoginController.java
@@ -0,0 +1,15 @@
+package login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+
+public class LoginController {
+    @FXML private Text actiontarget;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+}
